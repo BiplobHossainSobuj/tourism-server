3312a19aa0712087f8b88c4ff08dbf194ab7b75c
BiploHossainSobuj <bhs.cse@outlook.com>
2024-04-28 21:56:56 +0600

    develop delete operation from my list

--- index.js
+++ index.js
@@ -81,6 +81,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/delete/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await touristSpotCollection.deleteOne(query);
+      console.log(result);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
